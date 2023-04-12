bbdff2b85c06365f3500685bde4f40967f4307ae
Zachary Rojas <ztr2194@gmail.com>
2023-04-12 15:53:39 -0500

    lab done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const answer = document.getElementById('searchByID').value;
+    console.log (answer)
+
+    fetch(`http://localhost:3000/movies/${answer}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
